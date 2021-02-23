747b1412bb6065f69dcfe31569ed0020380b7334
nicop18 <nicolas.poitevin18@gmail.com>
2021-02-23 11:50:56 -0600

    desafio

--- mercadoliebre-dia-1/src/routes/api/itemsRouter.js
+++ mercadoliebre-dia-1/src/routes/api/itemsRouter.js
@@ -2,11 +2,11 @@ const express = require ('express')
 const router = express.Router();
 const apiItemsController = require('../../controllers/api/itemsController')
 const validator = require('../../middlewares/validator');
+const authMiddleware = require('../../middlewares/auth');
 
 
-
-router.post('/',  apiItemsController.addToCart) 
-router.delete('/', apiItemsController.deleteCar);
+router.post('/',authMiddleware, validator.addToCart,  apiItemsController.addToCart) 
+router.delete('/',authMiddleware, apiItemsController.deleteCart);
 
 
 module.exports = router;
